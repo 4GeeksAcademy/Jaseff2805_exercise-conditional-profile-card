5f6c2fcb95ba06ff67c9f270ea5a8f8d6f370a7e
Omar Jaseff Bautista Bazán <40043595+jaseff2805@users.noreply.github.com>
2025-04-26 05:32:59 +0000

    Actuialización de Proyecto

--- src/js/app.js
+++ src/js/app.js
@@ -29,21 +29,50 @@ function render(variables = {}) {
   let cover = `<div class="cover"><img src="${variables.background}" /></div>`;
   if (variables.includeCover == false) cover = "<div class='cover'></div>";
 
+  // Definición de variables de Datos Personales
+  const name = variables.name ? variables.name : "Put your name";
+  const lastName = variables.lastName
+    ? variables.lastName
+    : "- Put your Lastname";
+  const fullname = `${name} ${lastName}`.trim();
+  const role = variables.role ? variables.role : "";
+  const city = variables.city ? variables.city : "";
+  const country = variables.country ? variables.country : "";
+
+  // Definición de variables de Redes Sociales
+  const socialLinks = {
+    twitter: variables.twitter,
+    github: variables.github,
+    linkedin: variables.linkedin,
+    instagram: variables.instagram
+  };
+
+  // Creación del link de redes sociales dinámico. Se utiliza función flecha y los valores de los campos de redes sociales
+  const socialHTML = Object.entries(socialLinks)
+    .filter(([_, username]) => username)
+    .map(
+      ([platform, username]) => `
+  <li>
+    <a href="https://${platform}.com/${username}">
+      <i class="fab fa-${platform}"></i>
+    </a>
+  </li>`
+    )
+    .join("");
+
   // reset the website body with the new html output
-  document.querySelector("#widget_content").innerHTML = `<div class="widget">
-            ${cover}
-          <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/school/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
-          </ul>
-        </div>
-    `;
+  document.querySelector(
+    "#widget_content"
+  ).innerHTML = `<div class="widget">${cover}
+      <img src="${variables.avatarURL}" class="photo" />
+      <h1>${fullname}</h1>
+      <h2>${role}</h2>
+      <h3>${city}${city && country ? ", " : ""}${country}</h3>
+      <ul class="position-right">
+        ${socialHTML}
+      </ul>
+    </div>
+`;
 }
 
 /**
